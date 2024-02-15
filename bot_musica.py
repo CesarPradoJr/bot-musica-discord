d2fd65aa5fae7c12d024559fc419e470472928b8
Cesar <cesarhenrique_prado@hotmail.com>
2024-02-15 11:19:52 -0300

    feat: I added some rules in the bot and requested token request

--- bot_musica.py
+++ bot_musica.py
@@ -3,15 +3,34 @@ from discord.ext import commands
 from pytube import YouTube
 import json
 import datetime
+import os
 
 intents = discord.Intents.all()
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
+try:
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+        token = config.get('token')
+        if token is None:
+            raise ValueError('Token não encontrado no arquivo config.json')
+except FileNotFoundError:
+    print('Arquivo config.json não encontrado')
+    sys.exit(1)  # Termina a execução do programa
+except json.JSONDecodeError:
+    print('Erro ao decodificar o arquivo config.json')
+    sys.exit(1)  # Termina a execução do programa
+except ValueError as e:
+    print(e)
+    sys.exit(1)  # Termina a execução do programa
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 channel_id = 1181632907371094049
+
+FFMPEG_OPTIONS = {
+    'options': '-vn',  # Desativa a parte de vídeo do stream, deixando apenas o áudio
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',  # Reconecta se a conexão for perdida
+}
 
 @bot.event
 async def on_ready():
@@ -42,6 +61,10 @@ async def play(ctx, url):
         if not voice_channel or not voice_channel.is_connected():
             # Se não estiver conectado, conecte-se ao canal do autor da mensagem
             author_channel = ctx.author.voice.channel
+            if not author_channel:
+                await ctx.send("Você precisa estar em um canal de voz para usar este comando.")
+                return
+
             voice_channel = await author_channel.connect()
 
         # Criar objeto YouTube
@@ -51,10 +74,14 @@ async def play(ctx, url):
         audio_stream = yt.streams.filter(only_audio=True).first()
 
         # Baixar o áudio
-        audio_stream.download()
+        audio_file_path = os.path.join(os.getcwd(), audio_stream.title + ".mp4")
+        audio_stream.download(output_path=os.getcwd(), filename=audio_stream.title)
+        audio_source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source=audio_file_path, **FFMPEG_OPTIONS), volume=0.5)
 
         # Reproduzir o áudio no canal de voz
-        voice_channel.play(discord.FFmpegPCMAudio(audio_stream.title + ".webm"), after=lambda e: print('done', e))
+        print("Before playing audio")
+        await voice_channel.play(audio_source, after=lambda e: print('done', e))
+        print("After playing audio")
 
         await ctx.send(f'Tocando: {yt.title}')
 
@@ -68,4 +95,4 @@ async def stop(ctx):
     if voice_channel and voice_channel.is_connected():
         await voice_channel.disconnect()
 
-bot.run('token')
+bot.run(token)
